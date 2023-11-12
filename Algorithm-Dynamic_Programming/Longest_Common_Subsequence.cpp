a58d4d9ae30bf7c548e9bd1d82320dc9b8a7e077
Akhilesh <96287396+Akhilesh1004@users.noreply.github.com>
2023-11-12 16:28:16 +0800

    2023/11/12

--- Algorithm-Dynamic_Programming/Longest_Common_Subsequence.cpp
+++ Algorithm-Dynamic_Programming/Longest_Common_Subsequence.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string>
 #include <vector>
+#include <set>
 
 using namespace std;
 
@@ -17,6 +18,48 @@ int LCS(string a, string b){
         }
     }
     return lcs[n][m];
+}
+
+void solve(int i, int j, vector<char>&lcsword, vector<string>&result, string a, string b, vector<vector<int>>lcs){
+    if(i == 0 || j == 0){
+        if(lcsword.size() == lcs[a.size()][b.size()]){
+            string r = "";
+            for(int z = lcs[a.size()][b.size()]-1; z>=0; z--){
+                r += lcsword[z];
+            }
+            result.push_back(r);
+        }
+        return ;
+    }
+    if(a[i-1] == b[j-1]){
+        lcsword.push_back(a[i-1]);
+        solve(i-1, j-1, lcsword, result, a, b, lcs);
+        lcsword.pop_back();
+    }else{
+        if(lcs[i-1][j] >= lcs[i][j-1]){
+            solve(i-1, j, lcsword, result, a, b, lcs);
+        }
+        if(lcs[i][j-1] >= lcs[i-1][j]){
+            solve(i, j-1, lcsword, result, a, b, lcs);
+        }
+    }
+}
+
+vector<string> LCS_all(string a, string b){
+    int n = a.size();
+    int m = b.size();
+    vector<vector<int>>lcs(n+1, vector<int>(m+1, 0));
+    if(n == 0 || m == 0) return {};
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=m; j++){
+            if(a[i-1] == b[j-1]) lcs[i][j] = lcs[i-1][j-1]+1;
+            else lcs[i][j] = max(lcs[i-1][j], lcs[i][j-1]);
+        }
+    }
+    vector<char>lcsword;
+    vector<string>result;
+    solve(a.size(), b.size(), lcsword, result, a, b, lcs);
+    return result;
 }
 
 string LCS_word(string a, string b){
@@ -53,10 +96,17 @@ int main(){
     string a, b;
     while(getline(cin, a)){
         getline(cin, b);
+        /*
         int result = LCS(a, b);
         string sresult = LCS_word(a, b);
         cout<<result<<endl;
-        cout<<sresult<<endl;
+        cout<<sresult<<endl;*/
+        vector<string>result = LCS_all(a, b);
+        set<string>s(result.begin(), result.end());
+        result.assign(s.begin(), s.end());
+        for(int i = 0; i<result.size(); i++){
+            cout<<result[i]<<endl;
+        }
     }
     return 0;
 }
