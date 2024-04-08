544b841c70982165d8ea4d98919f2465b96a834a
Akhilesh <96287396+Akhilesh1004@users.noreply.github.com>
2024-04-08 16:39:56 +0800

    change

--- ds_oop/hw2/2.cpp
+++ ds_oop/hw2/2.cpp
@@ -1,32 +1,32 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-bool is_increasing(vector<int>&dp){
-    for(int i = 1; i<dp.size(); i++){
-        if(dp[i-1] > dp[i]) return false;
-    }
-    return true;
-}
-
-bool check(vector<int>&dp){
-    for(int i = 0; i<dp.size(); i++){
-        int temp = dp[i];
-        if(i > 0) dp[i] = dp[i-1];
-        else dp[i] = dp[i+1];
-        if(is_increasing(dp)) return true;
-        dp[i] = temp;
-    }
-    return false;
-}
-
-int main(){
-    int m;
-    cin>>m;
-    vector<int>dp(m);
-    for(int i = 0; i<m; i++) cin>>dp[i];
-    if(is_increasing(dp) || check(dp)) cout<<"YES\n";
-    else cout<<"NO\n";
-    return 0;
-}
+#include <iostream> 
+#include <vector> 
+ 
+using namespace std; 
+ 
+ 
+int main(){ 
+    int m; 
+    cin>>m; 
+    vector<int>dp(m); 
+    vector<int>check; 
+    int n = 1; 
+    for(int i = 0; i<m; i++) cin>>dp[i]; 
+    for(int i = 0; i<m; i++){ 
+        if(check.size() != 0 && check.back() == dp[i]){ 
+            check.pop_back(); 
+        }else{ 
+            while(n <= m && n != dp[i]){ 
+                check.push_back(n); 
+                n++; 
+            } 
+            if(n > m || n != dp[i]){ 
+                cout<<"NO\n"; 
+                return 0; 
+            } 
+            n++; 
+        } 
+    } 
+    if(check.size() == 0) cout<<"YES\n"; 
+    else cout<<"NO\n"; 
+    return 0; 
+} 
